e8fe4c55c400b93f4276a57104e84d14a41dccd1
Rogmer Bulaclac <rogmer.bulaclac@gmail.com>
2025-06-21 01:17:19 +0800

    Adding health check on the frontend

---
+++ frontend/sbtph-sales-leaderboard-app/pages/healthcheck.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>I am alive</h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'custom' ,// This tells Nuxt to use the 'custom' layout for this page
+//   middleware: ['auth'] 
+})
+</script>
